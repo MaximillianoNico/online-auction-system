23459e4ca69423f0bc89ff6b0e417ea2f5b982a0
MaximillianoNic <maximilliano@okadoc.com>
2023-03-02 07:08:27 +0700

    fix: fix submit deposit

--- apps/client/screens/deposit/actions.ts
+++ apps/client/screens/deposit/actions.ts
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
 const useDeposit = () => {
+  const [errors, setErrors] = useState("");
   const [deposit, setDeposit] = useState(0);
 
   const handleOnChange = (event: { target: HTMLInputElement; }) => setDeposit(+event?.target?.value);
   const handleSubmit = () => {
+    if (deposit <= 0) {
+      setErrors("Deposit should be more than 0")
+
+      return;
+    }
     // TODO:
   }
 
   return [
     handleSubmit,
-    { deposit, onChange: handleOnChange, onSubmit: handleSubmit }
+    { deposit, onChange: handleOnChange, onSubmit: handleSubmit, errors }
   ] as const
 }
 
